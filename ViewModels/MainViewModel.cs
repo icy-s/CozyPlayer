d3bd26f5c42d0d1057f9fc44fba816d36b93e43c
icy-s <evgeny.tailov@gmail.com>
2025-10-20 11:03:12 +0300

    baseviewmodel added

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -3,38 +3,55 @@ using System.Windows.Input;
 using CozyPlayer.Models;
 using CozyPlayer.Services;
 
-namespace CozyPlayer.ViewModels;
-
-public class MainViewModel : BaseViewModel
+namespace CozyPlayer.ViewModels
 {
-    private readonly DatabaseService _db;
-    private readonly AudioService _audio;
-
-    public ObservableCollection<Track> Tracks { get; set; } = new();
-
-    public ICommand PlayCommand { get; }
-    public ICommand DeleteCommand { get; }
-
-    public MainViewModel(DatabaseService db, AudioService audio)
+    public class MainViewModel : BaseViewModel
     {
-        _db = db;
-        _audio = audio;
+        private readonly DatabaseService _db;
+        private readonly AudioService _audio;
 
-        PlayCommand = new Command<Track>(async (track) => await _audio.PlayAsync(track.FilePath));
-        DeleteCommand = new Command<Track>(async (track) => await DeleteTrack(track));
-    }
+        public ObservableCollection<Track> Tracks { get; set; } = new();
 
-    public async Task LoadTracks()
-    {
-        Tracks.Clear();
-        var items = await _db.GetTracksAsync();
-        foreach (var i in items)
-            Tracks.Add(i);
-    }
+        public ICommand PlayCommand { get; }
+        public ICommand DeleteCommand { get; }
 
-    private async Task DeleteTrack(Track track)
-    {
-        await _db.DeleteTrackAsync(track);
-        Tracks.Remove(track);
+        public MainViewModel(DatabaseService db, AudioService audio)
+        {
+            _db = db;
+            _audio = audio;
+
+            PlayCommand = new Command<Track>(async (track) => await _audio.PlayAsync(track.FilePath));
+            DeleteCommand = new Command<Track>(async (track) => await DeleteTrack(track));
+        }
+
+        public async Task LoadTracks()
+        {
+            Tracks.Clear();
+            var items = await _db.GetTracksAsync();
+            foreach (var i in items)
+                Tracks.Add(i);
+        }
+
+        private async Task DeleteTrack(Track track)
+        {
+            await _db.DeleteTrackAsync(track);
+            Tracks.Remove(track);
+        }
+
+        public async Task MoveTrack(int fromIndex, int toIndex)
+        {
+            if (fromIndex == toIndex) return;
+
+            var item = Tracks[fromIndex];
+            Tracks.RemoveAt(fromIndex);
+            Tracks.Insert(toIndex, item);
+
+            // Обновляем порядок в базе данных
+            for (int i = 0; i < Tracks.Count; i++)
+            {
+                Tracks[i].Id = i; // Можно сделать отдельное поле Order
+                await _db.SaveTrackAsync(Tracks[i]);
+            }
+        }
     }
 }
--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -1,24 +1,49 @@
-﻿namespace CozyPlayer
+﻿using CozyPlayer.Models;
+using CozyPlayer.Services;
+using CozyPlayer.ViewModels;
+
+namespace CozyPlayer.Views;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    private MainViewModel ViewModel;
+    private Track _draggedTrack;
+
+    public MainPage()
     {
-        int count = 0;
+        InitializeComponent();
 
-        public MainPage()
+        var dbPath = Path.Combine(FileSystem.AppDataDirectory, "cozyplayer.db3");
+        var db = new DatabaseService(dbPath);
+        var audio = new AudioService();
+
+        ViewModel = new MainViewModel(db, audio);
+
+        BindingContext = ViewModel;
+
+        Task.Run(async () => await ViewModel.LoadTracks());
+    }
+
+    private void OnDragStarting(object sender, DragStartingEventArgs e)
+    {
+        if (sender is Grid grid && grid.BindingContext is Track track)
         {
-            InitializeComponent();
+            e.Data.Text = track.Id.ToString();
+            _draggedTrack = track;
+        }
+    }
+    private async void OnDrop(object sender, DropEventArgs e)
+    {
+        if (_draggedTrack == null) return;
+
+        if (sender is Grid grid && grid.BindingContext is Track targetTrack)
+        {
+            var from = ViewModel.Tracks.IndexOf(_draggedTrack);
+            var to = ViewModel.Tracks.IndexOf(targetTrack);
+
+            await ViewModel.MoveTrack(from, to);
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        _draggedTrack = null;
     }
 }
